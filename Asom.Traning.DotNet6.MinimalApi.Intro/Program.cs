706c7c5f2d7f01739f0af2b6eddc90f0776def28
Alfred Obialo <alfredcsdinc@gmail.com>
2022-04-27 23:46:10 +0100

    Add Simple Get request

--- Asom.Traning.DotNet6.MinimalApi.Intro/Program.cs
+++ Asom.Traning.DotNet6.MinimalApi.Intro/Program.cs
@@ -1,39 +1,31 @@
 ﻿var app = WebApplication.Create();
-
-
-
-app.UseRouting();
-app.UseAuthorization();
-app.UseEndpoints(config =>
+app.MapGet("asom", (IHostEnvironment service, ILogger<PhoneContact> logger) =>
 {
-    config.MapGet("asom", (IHostEnvironment service, ILogger<PhoneContact> logger) =>
+    logger.LogInformation("Testing Di in Minimal Api");
+    var data = new
     {
-        logger.LogInformation("Testing Di in Minimal Api");
-        var data = new
+        Env = new
         {
-            Env  = new
-            {
-                mode = service.EnvironmentName,
-                appName = service.ApplicationName,
-                appPath =service.ContentRootPath
-            },
-            name = new
-            {
-                firstName = "Alfred",
-                lastName = "Obialo",
-                middleName = "Ikechukwu"
-            },
-            age = 34,
-            phoneContact = new List<PhoneContact>
-            {
-                new() { Tag = "Primary", Number = "08069273479" },
-                new() { Tag = "Office", Number = "08039973479" },
-                new() { Tag = "Pager", Number = "09089973479" },
-            },
-            gender = "Male"
-        };
-        return Results.Ok(data);
-    }).RequireAuthorization();
+            mode = service.EnvironmentName,
+            appName = service.ApplicationName,
+            appPath = service.ContentRootPath
+        },
+        name = new
+        {
+            firstName = "Alfred",
+            lastName = "Obialo",
+            middleName = "Ikechukwu"
+        },
+        age = 34,
+        phoneContact = new List<PhoneContact>
+        {
+            new() { Tag = "Primary", Number = "08069273479" },
+            new() { Tag = "Office", Number = "08039973479" },
+            new() { Tag = "Pager", Number = "09089973479" },
+        },
+        gender = "Male"
+    };
+    return Results.Ok(data);
 });
 
 app.Run("http://localhost:7111");
